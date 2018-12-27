14c0bca0be652ca19961d0821fadf667f23307ed
chainsafe <aidan@chainsafe.io>
2018-12-27 15:09:53 -0500

    added license headers

--- tests/Int.test.ts
+++ tests/Int.test.ts
@@ -1,3 +1,19 @@
+//Fixed Sized Numbers TS
+ //Copyright (C) 2018 ChainSafe Systems
+
+  // This program is free software: you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation, either version 3 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import BigNumber from "bignumber.js";
 import { assert, expect } from "chai";
 import "mocha";
@@ -1072,4 +1088,3 @@ describe("SafeMath tests for Int64", () => {
         assert(false, "Int64 multiplication should not overflow");
     });
 });
-
