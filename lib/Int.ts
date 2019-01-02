9150ffb42d46fed129e9fcf893cbda78ecd329de
David Ansermino <david@chainsafe.io>
2019-01-02 08:55:49 +0200

    Removed ability to instantiate with primitive numbers (#9)
    All types must be instantiated with a String or BigNumber

--- lib/Int.ts
+++ lib/Int.ts
@@ -33,17 +33,17 @@ const IntFactory = <T>(size: number) => (identifier: object) => pipe(
     Object.freeze,
 );
 
-const Int8 = (value?: number | string | BigNumber): Int8 => IntFactory<Int8>(8)({_int8: true})(value);
+const Int8 = (value?: string | BigNumber): Int8 => IntFactory<Int8>(8)({_int8: true})(value);
 
-const Int16 = (value?: number | string | BigNumber): Int16 => IntFactory<Int16>(16)({_int16: true})(value);
+const Int16 = (value?: string | BigNumber): Int16 => IntFactory<Int16>(16)({_int16: true})(value);
 
-const Int32 = (value?: number | string | BigNumber): Int32 => IntFactory<Int32>(32)({_int32: true})(value);
+const Int32 = (value?: string | BigNumber): Int32 => IntFactory<Int32>(32)({_int32: true})(value);
 
-const Int64 = (value?: string): Int64 => IntFactory<Int64>(64)({_int64: true})(value);
+const Int64 = (value?: string | BigNumber): Int64 => IntFactory<Int64>(64)({_int64: true})(value);
 
-const Int128 = (value?: string): Int128 => IntFactory<Int128>(128)({_int128: true})(value);
+const Int128 = (value?: string | BigNumber): Int128 => IntFactory<Int128>(128)({_int128: true})(value);
 
-const Int256 = (value?: string): Int256 => IntFactory<Int256>(256)({_int256: true})(value);
+const Int256 = (value?: string | BigNumber): Int256 => IntFactory<Int256>(256)({_int256: true})(value);
 
 // Type Checkers
 const isInt8 = (x: Int): x is Int8 => (x as Int8)._int8;
